9f71bdc72f8cfd44b6d73a65440bc65cf6e40c7d
xiaojun123 <359945657@qq.com>
2021-07-08 02:30:25 +0800

    封装双向链表

--- AssetBundleConfig/Assets/Scripts/ResourceManager.cs
+++ AssetBundleConfig/Assets/Scripts/ResourceManager.cs
@@ -202,3 +202,115 @@ public class DoubleLinkList<T> where T : class, new()
 
     }
 }
+
+public class CMapList<T> where T : class, new()
+{
+    DoubleLinkList<T> m_DLink = new DoubleLinkList<T>();
+    Dictionary<T, DoubleLinkListNode<T>> m_FindMap = new Dictionary<T, DoubleLinkListNode<T>>();
+
+    ~CMapList()
+    {
+        Clear();
+    }
+
+    /// <summary>
+    /// 清空列表
+    /// </summary>
+    public void Clear()
+    {
+        while (m_DLink.Tail != null)
+        {
+            Remove(m_DLink.Tail.t);
+        }
+    }
+
+    /// <summary>
+    /// 插入一个节点到表头
+    /// </summary>
+    /// <param name="t"></param>
+    public void InsertToHead(T t)
+    {
+        DoubleLinkListNode<T> node = null;
+        if (m_FindMap.TryGetValue(t, out node) && node != null)
+        {
+            m_DLink.AddToHeader(node);
+            return;
+        }
+        m_DLink.AddToHeader(t);
+        m_FindMap.Add(t, m_DLink.Head);
+    }
+
+    /// <summary>
+    /// 从表尾弹出节点
+    /// </summary>
+    public void Pop()
+    {
+        if (m_DLink.Tail != null)
+        {
+            Remove(m_DLink.Tail.t);
+        }
+    }
+
+    public void Remove(T t)
+    {
+        DoubleLinkListNode<T> node = null;
+        if (!m_FindMap.TryGetValue(t, out node)||node==null)
+        {
+            return;
+        }
+        m_DLink.RemoveNode(node);
+        m_FindMap.Remove(t);
+    }
+
+    /// <summary>
+    /// 获取尾部节点
+    /// </summary>
+    /// <returns></returns>
+    public T Back()
+    {
+        return m_DLink.Tail == null ? null : m_DLink.Tail.t;
+    }
+
+    /// <summary>
+    /// 返回节点个数
+    /// </summary>
+    /// <returns></returns>
+    public int Size()
+    {
+        return m_FindMap.Count;
+    }
+
+    /// <summary>
+    /// 查找是否存在该节点
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public bool Find(T t)
+    {
+        DoubleLinkListNode<T> node = null;
+        if (!m_FindMap.TryGetValue(t, out node) || node == null)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// 刷新某个节点把节点移动到头部
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public bool Refresh(T t)
+    {
+        DoubleLinkListNode<T> node = null;
+        if (!m_FindMap.TryGetValue(t, out node) || node == null)
+        {
+            return false;
+        }
+        m_DLink.MoveToHead(node);
+        return true;
+    }
+}
