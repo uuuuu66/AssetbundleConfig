20b22f50c24307e00f015b005fa51a3d2050249e
xiaojun123 <359945657@qq.com>
2021-07-29 03:25:47 +0800

    添加离线数据，用于记录对象池里对象状态，以便还原

---
+++ AssetBundleConfig/Assets/Scripts/OfflineData/OfflineData.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfflineData : MonoBehaviour
+{
+    public Rigidbody m_Rigibody;
+    public Collider m_Collider;
+    //所有节点
+    public Transform[] m_AllPoint;
+    //节点下子节点个数
+    public int[] m_AllPointChildCount;
+    //每个节点是否激活
+    public bool[] m_AllPointActive;
+    //每个节点位置信息
+    public Vector3[] m_Pos;
+    //每个节点大小信息
+    public Vector3[] m_Scale;
+    //每个节点旋转信息
+    public Quaternion[] m_Rot;
+
+    /// <summary>
+    /// 还原属性
+    /// </summary>
+    public virtual void ResetProp()
+    {
+        int allPointCount = m_AllPoint.Length;
+        for (int i = 0; i < allPointCount; i++)
+        {
+            Transform tempTrs = m_AllPoint[i];
+            if (tempTrs != null)
+            {
+                tempTrs.localPosition = m_Pos[i];
+                tempTrs.localRotation = m_Rot[i];
+                tempTrs.localScale = m_Scale[i];
+
+                if (m_AllPointActive[i])
+                {
+                    if (!tempTrs.gameObject.activeSelf)
+                    {
+                        tempTrs.gameObject.SetActive(true);
+                    }
+                }
+                else
+                {
+                    if (tempTrs.gameObject.activeSelf)
+                    {
+                        tempTrs.gameObject.SetActive(false);
+                    }
+                }
+
+                if (tempTrs.childCount > m_AllPointChildCount[i])
+                {
+                    int childCount = tempTrs.childCount;
+                    for (int j = m_AllPointChildCount[i]; j < childCount; j++)
+                    {
+                        GameObject tempObj = tempTrs.GetChild(j).gameObject;
+                        if (!ObjectManager.Instance.IsObjectManagerCreat(tempObj))
+                        {
+                            GameObject.Destroy(tempObj);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    ///  编辑器下保存初始数据
+    /// </summary>
+    public virtual void BindData()
+    {
+        m_Rigibody = gameObject.GetComponentInChildren<Rigidbody>(true);
+        m_Collider = gameObject.GetComponentInChildren<Collider>(true);
+        m_AllPoint = gameObject.GetComponentsInChildren<Transform>(true);
+        int allPointCount = m_AllPoint.Length;
+        m_AllPointChildCount = new int[allPointCount];
+        m_AllPointActive = new bool[allPointCount];
+        m_Pos = new Vector3[allPointCount];
+        m_Rot = new Quaternion[allPointCount];
+        m_Scale = new Vector3[allPointCount];
+        for (int i = 0; i < allPointCount; i++)
+        {
+            Transform temp = m_AllPoint[i] as Transform;
+            m_AllPointChildCount[i] = temp.childCount;
+            m_AllPointActive[i] = temp.gameObject.activeSelf;
+            m_Pos[i] = temp.localPosition;
+            m_Rot[i] = temp.localRotation;
+            m_Scale[i] = temp.localScale;
+        }
+    }
+}
